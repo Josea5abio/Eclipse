e8fdf57c3f963997960c9204a3f540657a496970
Josea5abio <joseantsabiomolina@gmail.com>
2023-03-21 08:41:09 +0100

    fix: new variable

--- src/database/DbConnection.java
+++ src/database/DbConnection.java
@@ -8,8 +8,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+// 
 public class DbConnection {
+	public int prueba;
     private Connection con;
     private static DbConnection dbc;
     private DbConnection() {
